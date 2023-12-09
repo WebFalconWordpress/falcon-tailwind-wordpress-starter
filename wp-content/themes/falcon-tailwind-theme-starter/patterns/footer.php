74a55ed20cebcb40bd89eda6330a9206db2dd2ad
Danilo Puric <danilo@bild-studio.net>
2023-12-09 19:09:13 +0100

    Removed forced font size that we are not using any more in our starter

--- wp-content/themes/falcon-tailwind-theme-starter/patterns/footer.php
+++ wp-content/themes/falcon-tailwind-theme-starter/patterns/footer.php
@@ -14,8 +14,8 @@
 		class="container flex justify-between h-20 mx-auto text-center text-gray-500 wp-block-group place-items-center">
 		<!-- wp:site-logo /-->
 
-		<!-- wp:paragraph {"className":"has-text-align-center has-secondary-color has-text-color has-link-color has-small-font-size"} -->
-		<p class="has-text-align-center has-secondary-color has-text-color has-link-color has-small-font-size">
+		<!-- wp:paragraph {"className":"has-text-align-center has-secondary-color has-text-color has-link-color "} -->
+		<p class="has-text-align-center has-secondary-color has-text-color has-link-color ">
 		&copy; <?php echo date_i18n('Y'); ?> - <?php echo get_bloginfo('name'); ?> </p>
 		<!-- /wp:paragraph -->
 	</div>
